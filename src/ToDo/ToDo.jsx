4bed66e3b15a59d1b602aed67832c868af0b12c5
AleksandrKompanetc <moneymacker7@gmail.com>
2024-07-31 22:37:26 +0300

    added the ability to add tasks

--- src/ToDo/ToDo.jsx
+++ src/ToDo/ToDo.jsx
@@ -2,27 +2,46 @@ import React, { useState } from 'react';
 import './index.css';
 import ToDoItem from './ToDoItem';
 
-const toDoBase = [{
-  _id: 1,
-  name: 'Buy MacBook',
-  isChecked: true,
-},
-{
-  _id: 2,
-  name: 'Go to work',
-  isChecked: false,
-}]
-
 const ToDo = () => {
   const [name, setName] = useState('');
+  const [todos, setTodos] = useState([
+    {
+      _id: 0,
+      name: 'Buy MacBook',
+      isChecked: true,
+    },
+  ]);
+
+  const onKeyPressNameHandler = e => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      setTodos(prev => [...prev, { _id: todos.length, name, isChecked: false }])
+      setName('');
+    }
+  }
+
+  const toggleCheckedToDo = idx => {
+    const newArray = [].concat(todos);
+    newArray[idx].isChecked = !newArray[idx].isChecked;
+    setTodos(newArray);
+  }
+
   return (
     <>
       <h1>ToDo App</h1>
-      {toDoBase.map(todo => <ToDoItem key={`_todo_${todo._id}`} name={todo.name} isChecked={todo.isChecked} />)}
+      {todos &&
+        todos.map((todo, idx) => <ToDoItem
+          key={`_todo_${todo._id}`}
+          idx={idx}
+          name={todo.name}
+          isChecked={todo.isChecked}
+          toggleCheckedToDo={toggleCheckedToDo}
+        />)}
       <input
         type='text'
         value={name}
         onChange={e => setName(e.target.value)}
+        onKeyPress={onKeyPressNameHandler}
         placeholder='Enter new task...'
       />
     </>
